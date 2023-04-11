baf3db0d18f9b7de177e7660041ccf3b52c3ba63
anafreis <72563090+anafreis@users.noreply.github.com>
2023-04-11 11:48:31 -0300

    Add files via upload

--- Matlab_codes/plotting_results.m
+++ Matlab_codes/plotting_results.m
@@ -56,8 +56,32 @@ memory_Compensated_LSTM_DPA_TA_ERR = ERR_LSTM_DPA_TA;
 clearvars  scenario pathdata BER_Ideal BER_DPA_LSTM_DNN ERR_DPA_LSTM_DNN...
     BER_LSTM_NN_DPA ERR_LSTM_NN_DPA BER_LSTM_DPA_TA ERR_LSTM_DPA_TA
 
-%
+%% Normalized Mean Squared Error
 figure(1)
+colorOrder = get(gca, 'ColorOrder');
+LSTMDPANN_nonCompensated = semilogy(EbN0dB,memory_woCompensation_LSTM_DPA_NN_ERR ,'-o','MarkerFaceColor',colorOrder(1,:),'color',colorOrder(1,:),'MarkerSize',8,'LineWidth',2);
+hold on;
+LSTMDPATA_nonCompensated = semilogy(EbN0dB,memory_woCompensation_LSTM_DPA_TA_ERR ,'-^','MarkerFaceColor',colorOrder(2,:),'color',colorOrder(2,:),'MarkerSize',8,'LineWidth',2);
+DPALSTMNN_nonCompensated = semilogy(EbN0dB,memory_woCompensation_DPA_LSTM_NN_ERR,'-h','MarkerFaceColor',colorOrder(3,:),'color',colorOrder(3,:),'MarkerSize',8,'LineWidth',2);
+grid on
+set(0,'defaulttextinterpreter','latex')
+set(groot,'defaultAxesTickLabelInterpreter','latex')
+hAxes.TickLabelInterprete = 'latex';
+set(0,'DefaultTextFontname', 'CMU Serif')
+set(0,'DefaultAxesFontName', 'CMU Serif')
+xlabel('SNR ($\xi$) [dB]');
+ylabel('NMSE');
+set(gca,'FontSize',18)
+if strcmp(mod,'16QAM') && IBO == 2 
+    axis([min(EbN0dB) max(EbN0dB) 10^-3 1])
+    yticks([10^-3 10^-2 10^-1 10^0])
+else
+    axis([min(EbN0dB) max(EbN0dB) 10^-3 1])
+    yticks([10^-3 10^-2 10^-1 10^0])
+end
+
+%% Bit Error Rate
+figure(2)
 subplot(1,3,1)
 colorOrder = get(gca, 'ColorOrder');
 semilogy(EbN0dB, memoryless_ideal,'k--','LineWidth',2);
@@ -82,7 +106,7 @@ else
     yticks([10^-4 10^-3 10^-2 10^-1 10^0])
 end
 title('Memoryless','FontSize',16)
-%%
+
 subplot(1,3,2)
 colorOrder = get(gca, 'ColorOrder');
 semilogy(EbN0dB, memory_woCompensation_ideal,'k--','LineWidth',2);
@@ -107,7 +131,7 @@ else
     yticks([10^-4 10^-3 10^-2 10^-1 10^0])
 end
 title('Memory','FontSize',16)
-%%
+
 subplot(1,3,3)
 colorOrder = get(gca, 'ColorOrder');
 semilogy(EbN0dB, memory_Compensated_ideal,'k--','LineWidth',2);
@@ -130,78 +154,5 @@ if strcmp(mod,'16QAM') && IBO == 2
 else
     axis([min(EbN0dB) max(EbN0dB) 10^-4 1])
     yticks([10^-4 10^-3 10^-2 10^-1 10^0])
-end
-title('Memory Compensated','FontSize',16)
-%% Normalized Mean Squared error (NMSE)
-figure(2)
-subplot(1,3,1)
-colorOrder = get(gca, 'ColorOrder');
-LSTMDPANN_memlss = semilogy(EbN0dB, memoryless_LSTM_DPA_NN_ERR,'-o','MarkerFaceColor',colorOrder(1,:),'color',colorOrder(1,:),'MarkerSize',8,'LineWidth',2);
-hold on
-LSTMDPATA_memlss = semilogy(EbN0dB, memoryless_LSTM_DPA_TA_ERR,'-^','MarkerFaceColor',colorOrder(2,:),'color',colorOrder(2,:),'MarkerSize',8,'LineWidth',2);
-DPALSTMNN_memlss = semilogy(EbN0dB, memoryless_DPA_LSTM_NN_ERR,'-h','MarkerFaceColor',colorOrder(3,:),'color',colorOrder(3,:),'MarkerSize',8,'LineWidth',2);
-grid on
-set(0,'defaulttextinterpreter','latex')
-set(groot,'defaultAxesTickLabelInterpreter','latex')
-hAxes.TickLabelInterprete = 'latex';
-set(0,'DefaultTextFontname', 'CMU Serif')
-set(0,'DefaultAxesFontName', 'CMU Serif')
-xlabel('SNR ($\xi$) [dB]');
-ylabel('NMSE');
-set(gca,'FontSize',18)
-if strcmp(mod,'16QAM') && IBO == 2 
-    axis([min(EbN0dB) max(EbN0dB) 10^-3 1])
-    yticks([10^-3 10^-2 10^-1 10^0])
-else
-    axis([min(EbN0dB) max(EbN0dB) 10^-3 1])
-    yticks([10^-3 10^-2 10^-1 10^0])
-end
-title('Memoryless','FontSize',16)
-
-subplot(1,3,2)
-colorOrder = get(gca, 'ColorOrder');
-LSTMDPANN_nonCompensated = semilogy(EbN0dB,memory_woCompensation_LSTM_DPA_NN_ERR ,'-o','MarkerFaceColor',colorOrder(1,:),'color',colorOrder(1,:),'MarkerSize',8,'LineWidth',2);
-hold on;
-LSTMDPATA_nonCompensated = semilogy(EbN0dB,memory_woCompensation_LSTM_DPA_TA_ERR ,'-^','MarkerFaceColor',colorOrder(2,:),'color',colorOrder(2,:),'MarkerSize',8,'LineWidth',2);
-DPALSTMNN_nonCompensated = semilogy(EbN0dB,memory_woCompensation_DPA_LSTM_NN_ERR,'-h','MarkerFaceColor',colorOrder(3,:),'color',colorOrder(3,:),'MarkerSize',8,'LineWidth',2);
-grid on
-set(0,'defaulttextinterpreter','latex')
-set(groot,'defaultAxesTickLabelInterpreter','latex')
-hAxes.TickLabelInterprete = 'latex';
-set(0,'DefaultTextFontname', 'CMU Serif')
-set(0,'DefaultAxesFontName', 'CMU Serif')
-xlabel('SNR ($\xi$) [dB]');
-ylabel('NMSE');
-set(gca,'FontSize',18)
-if strcmp(mod,'16QAM') && IBO == 2 
-    axis([min(EbN0dB) max(EbN0dB) 10^-3 1])
-    yticks([10^-3 10^-2 10^-1 10^0])
-else
-    axis([min(EbN0dB) max(EbN0dB) 10^-3 1])
-    yticks([10^-3 10^-2 10^-1 10^0])
-end
-title('Memory','FontSize',16)
-
-subplot(1,3,3)
-colorOrder = get(gca, 'ColorOrder');
-LSTMDPANN_memory_Comp = semilogy(EbN0dB,memory_Compensated_LSTM_DPA_NN_ERR ,'-o','MarkerFaceColor',colorOrder(1,:),'color',colorOrder(1,:),'MarkerSize',8,'LineWidth',2);
-hold on;
-LSTMDPATA_memory_Comp = semilogy(EbN0dB,memory_Compensated_LSTM_DPA_TA_ERR ,'-^','MarkerFaceColor',colorOrder(2,:),'color',colorOrder(2,:),'MarkerSize',8,'LineWidth',2);
-DPALSTMNN_memory_Comp = semilogy(EbN0dB,memory_Compensated_DPA_LSTM_NN_ERR,'-h','MarkerFaceColor',colorOrder(3,:),'color',colorOrder(3,:),'MarkerSize',8,'LineWidth',2);
-grid on
-set(0,'defaulttextinterpreter','latex')
-set(groot,'defaultAxesTickLabelInterpreter','latex')
-hAxes.TickLabelInterprete = 'latex';
-set(0,'DefaultTextFontname', 'CMU Serif')
-set(0,'DefaultAxesFontName', 'CMU Serif')
-xlabel('SNR ($\xi$) [dB]');
-ylabel('NMSE');
-set(gca,'FontSize',18)
-if strcmp(mod,'16QAM') && IBO == 2 
-    axis([min(EbN0dB) max(EbN0dB) 10^-3 1])
-    yticks([10^-3 10^-2 10^-1 10^0])
-else
-    axis([min(EbN0dB) max(EbN0dB) 10^-3 1])
-    yticks([10^-3 10^-2 10^-1 10^0])
 end
 title('Memory Compensated','FontSize',16)
